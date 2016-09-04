a4975d68eedbe7b34d9386bd60176e2a6f5d7580
pepsikirk <pepsikirk@qq.com>
2016-09-04 10:43:42 +0800

    Initial Commit

---
+++ GitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub
+//
+//  Created by pepsikirk on 16/9/4.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by pepsikirk on 16/9/4.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
